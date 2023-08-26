856a64aa6de449780800a16dfb54574d6ba51b9a
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2023-08-26 14:35:09 +0200

    Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import drone from'../images/telloDrone.png'
+
+const Hero = () => {
+  return (
+    <div className='text-white '>
+
+
+        <div className=' float-left max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+            <p className=' text-[#EB1402] font-bold p-2'>  Securing.......</p>
+            <h1 className='md:text-8xl sm:text-6xl text-4xl font-bold md:py-6' >Drone Project</h1>
+            <p className='text-xl font-semibold'>A project by <span className='text-[#32CD32]'>Group 5</span></p>
+
+
+        </div>
+        <div className=' mx-auto flex flex-col justify-center'>
+            <img className=' m-w-[100px] m-h-[100px] object-none ' src={drone} alt='tello drone' />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+    <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white '>
         <h1 className='w-full text-3xl font-bold text-[#32CD32]'>Group 5</h1>
         <ul className='flex'>
             <li className='p-4'>Home</li>
