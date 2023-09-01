662fb27b6095e422cc077fe29edd5155d4873e19
Jacques van Zyl <578624@student.belgiumcampus.ac.za>
2023-09-01 20:15:14 +0200

    Updated Colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,8 @@ const Navbar = () => {
     
     <div className='max-w-[auto] bg-[#051026] w-full fixed mt-[-10px]'>
     <div className='flex justify-between items-center h-24 max-w-[80%] mx-auto px-4 text-white bg-[#051026]'>
-        <h1 className='w-auto pl-[30px] pr-[30px] text-3xl font-bold text-[#D3E1FF] bg-[#0A2254] square border-2 border-s-0 border-t-0 p-3' style={{borderRadius: '22.5px', borderColor: '#133F9C'}}>Home Page</h1>
-        <ul className='md:flex bg-[#0A2254] w-auto h-[60px] text-[#D3E1FF] border-2 border-s-0 border-t-0' style={{borderRadius: '22.5px', borderColor: '#133F9C'}}>
+        <h1 className='w-auto pl-[30px] pr-[30px] text-3xl font-bold text-[#D3E1FF] bg-[#051026] square border-2 border-e-0 border-t-0 p-3' style={{borderRadius: '22.5px', borderColor: '#081B40'}}>Home Page</h1>
+        <ul className='md:flex bg-[#051026] w-auto h-[60px] text-[#D3E1FF] border-2 border-s-0 border-t-0' style={{borderRadius: '22.5px', borderColor: '#081B40'}}>
             <li className='p-4 mt-[auto] mb-[auto] pt-[1%] pl-6 border-e h-8'>Home</li>
             <li className='p-4 mt-[auto] mb-[auto] pt-[1%] border-e h-8'>About</li>
             <li className='p-4 mt-[auto] mb-[auto] pt-[1%] border-e h-8'>Log In</li>
