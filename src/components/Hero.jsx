5149db8e05b18c88ff673a16068882789b4d549f
Jacques van Zyl <578624@student.belgiumcampus.ac.za>
2023-09-01 19:03:47 +0200

    Fixed top bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,14 @@ const Hero = () => {
     <div className='text-white '>
 
         {/* float-left */}
-        <div className='  max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-            <img className=' max-w-[750px] max-h-[600px] object-none ' src={drone} alt='tello drone' />
 
-            <p className=' text-[#EB1402] font-bold p-2'>  Securing.......</p>
+            <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center' style={{marginTop: '10px'}}>
             <h1 className='md:text-8xl sm:text-6xl text-4xl font-bold md:py-6' >Drone Project</h1>
             <p className='text-xl font-semibold'>A project by <span className='text-[#32CD32]'>Group 5</span></p>
-
+            <img className=' max-w-[750px] max-h-[600px] object-none ' src={drone} alt='tello drone' />
+{/*
+            <p className=' text-[#EB1402] font-bold p-2'>  Securing.......</p>
+*/}
 
         </div>
         {/* <div className=' mx-auto flex flex-col justify-center'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,15 @@ const Navbar = () => {
   }
 
   return (
-    <div className=' flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white '>
-        <h1 className='w-full text-3xl font-bold text-[#32CD32]'>Group 5</h1>
-        <ul className='hidden md:flex'>
-            <li className='p-4'>Home</li>
-            <li className='p-4'>About</li>
-            <li className='p-4'>LogIn</li>
-            <li className='p-4'>SignUp</li>
+    
+    <div className='max-w-[auto] bg-[#051026] w-full fixed mt-[-10px]'>
+    <div className='flex justify-between items-center h-24 max-w-[80%] mx-auto px-4 text-white bg-[#051026]'>
+        <h1 className='w-auto pl-[30px] pr-[30px] text-3xl font-bold text-[#D3E1FF] bg-[#0A2254] square border-2 border-s-0 border-t-0 p-3' style={{borderRadius: '22.5px', borderColor: '#133F9C'}}>Home Page</h1>
+        <ul className='md:flex bg-[#0A2254] w-auto h-[60px] text-[#D3E1FF] border-2 border-s-0 border-t-0' style={{borderRadius: '22.5px', borderColor: '#133F9C'}}>
+            <li className='p-4 mt-[auto] mb-[auto] pt-[1%] pl-6 border-e h-8'>Home</li>
+            <li className='p-4 mt-[auto] mb-[auto] pt-[1%] border-e h-8'>About</li>
+            <li className='p-4 mt-[auto] mb-[auto] pt-[1%] border-e h-8'>Log In</li>
+            <li className='p-4 mt-[auto] mb-[auto] pt-[1%] pr-5 h-8'>Sign Up</li>
 
         </ul>
 
@@ -38,6 +40,7 @@ const Navbar = () => {
 
         </div> */}
 
+    </div>
     </div>
   )
 }
