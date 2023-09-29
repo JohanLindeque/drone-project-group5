e1203be115001a0c0c1be09cf3afbc296f59836a
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2023-09-29 12:29:57 +0200

    Able to add new user

--- Backend/server.js
+++ Backend/server.js
@@ -11,19 +11,44 @@ const db = mysql.createConnection({
     password: "root123",
     database: "prg381db"
 
-})
+});
 
 app.get('/', (req,res)=>{
     res.json({message: 'Welcome to the PRG381 project!'})
-})
+});
 
+
+// GET all users
 app.get('/users', (req,res)=>{
     const q = "SELECT * FROM users";
     db.query(q, (err, data)=>{
         if(err) return res.json(err);
         res.json(data)
     })
-})
+});
+
+//  Add a new user
+app.post('/users', (req,res)=>{
+    
+    const q = "INSERT INTO users (`email`,`password`) VALUES (?)";
+    const values = ["backend@email","backend"];
+
+
+    db.query( q, [values], (err, data)=>{
+        if(err) return res.json(err);
+        return res.json("data inserted successfully");
+    })
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
