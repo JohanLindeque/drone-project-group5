47e7650e9556524e195c02ee6659bc8fa6df4b0c
JD-Faurie <143090453+JD-Faurie@users.noreply.github.com>
2023-10-09 12:14:37 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,57 +1,57 @@
-import express from 'express';
-import mysql from 'mysql';
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
 
 const app = express();
+const port = 5000;
 
-// connect to prg381db database
+// Create a MySQL database connection
 const db = mysql.createConnection({
-
-    host: "localhost",
-    user: "root",
-    password: "root123",
-    database: "prg381db"
-
+  host: 'localhost', 
+  user: 'root', 
+  password: 'password', 
+  database: 'prj381', 
 });
 
-app.get('/', (req,res)=>{
-    res.json({message: 'Welcome to the PRG381 project!'})
+app.use(express.json());
+app.use(cors());
+
+// API endpoint to fetch all users
+app.get('/users', (req, res) => {
+  const q = "SELECT * FROM users"
+  db.query(q, (err, data) => {
+    if (err) {
+      console.error('Error fetching users:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json(data);
+  });
 });
 
+// API endpoint to create a new user
+app.post('/users', (req, res) => {
+  const q = "INSERT INTO users (User_email, User_password) VALUES (?)";
+  const values = [
+    req.body.email,
+    req.body.password
+  ];
+  if (!values) {
+    res.status(400).json({ error: 'Text is required' });
+    return;
+  }
 
-// GET all users
-app.get('/users', (req,res)=>{
-    const q = "SELECT * FROM users";
-    db.query(q, (err, data)=>{
-        if(err) return res.json(err);
-        res.json(data)
-    })
+  db.query(q, [values], (err, result) => {
+    if (err) {
+      console.error('Error creating todo:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json({ message: 'User created successfully', id: result.insertId });
+  });
 });
 
-//  Add a new user
-app.post('/users', (req,res)=>{
-    
-    const q = "INSERT INTO users (`email`,`password`) VALUES (?)";
-    const values = ["backend@email","backend"];
-
-
-    db.query( q, [values], (err, data)=>{
-        if(err) return res.json(err);
-        return res.json("data inserted successfully");
-    })
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(8800, () =>{
-    console.log('Backend server is running! ')
-})
