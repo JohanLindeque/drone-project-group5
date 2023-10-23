e216ff33a1fdebd36b1c3abf289eecc4da21bcfe
JD-Faurie <143090453+JD-Faurie@users.noreply.github.com>
2023-10-23 09:22:51 +0200

    backend, login and signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,107 +1,74 @@
-import React, { useState } from 'react';
-
-const handleLogin = (formData) => {
-  // THIS IS FOR DEV PURPOSES ONLY
-  if (formData.email === 'admin@email' && formData.password === 'admin') {
-    // Redirect to /dashboard using window.location.href
-    window.location.href = '/dashboard';
-  } else {
-    // incorect credentials
-    alert('Invalid credentials. Please try again.'); 
-  }
-};
+import React, { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/authContext';
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+  
+    const [user, setUser] = useState({
+        email:"",
+        password:"",
     });
-  };
+    
+    const [err, setError] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const navigate = useNavigate();
 
-    // Call the login function with form data
-    handleLogin(formData);
-  };
+    const {login} = useContext(AuthContext);
 
+    const handleChange = (e) =>{
+        setUser(prev=>({...prev, [e.target.name]: e.target.value}))
+    }
 
-  return (
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div className="w-full bg-white rounded-lg shadow dark md:mt-0 sm:max-w-md xl:p-0">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-[#32CD32] md:text-2xl">
-            Login
-          </h1>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Your email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                placeholder="name@company.com"
-                required
-              />
+    const handleClick = async e =>{
+        e.preventDefault();
+        if (!user.email || !user.password) {
+            setError('Email and password are required.');
+            return;
+          }
+        try {
+            await login(user);
+            navigate("/dashboard");
+
+        } catch (err) {
+            setError(err.response.data.error);
+        }
+    }
+    return (
+        
+    <div  class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+        <div class="w-full bg-white rounded-lg shadow dark md:mt-0 sm:max-w-md xl:p-0 ">
+
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-[#32CD32]  md:text-2xl " >Login</h1>
+                <form>
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                        <input type="email" onChange={handleChange} name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required="" />
+                    </div>
+
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                        <input type="password" onChange={handleChange} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
+                    </div>
+
+                    <br />
+                    <div>   
+                        <button type="submit" onClick={handleClick} class="w-full text-white bg-[#0E327A] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Login</button>
+                        {err && <p class="text-center text-[#ff0000]">{err}</p>}
+                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            Don’t have an account yet? <a href="/signup" class="font-medium text-[#0E327A] hover:underline ">Sign up</a>
+                        </p>
+                    </div>
+
+                </form>
             </div>
 
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={formData.password}
-                onChange={handleInputChange}
-                placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                required
-              />
-            </div>
-
-            <br />
-            <div>
-              <button
-                type="submit"
-                className="w-full text-white bg-[#0E327A] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Sign in
-              </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{' '}
-                <a
-                  href="/signup"
-                  className="font-medium text-[#EB1402] hover:underline"
-                >
-                  Sign up
-                </a>
-              </p>
-            </div>
-          </form>
         </div>
-      </div>
-    </div>
-  );
-};
 
-export default Login;
+    </div>
+
+  )
+}
+
+export default Login
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
-  return (
+   
+    const [user, setUser] = useState({
+        email:"",
+        password:"",
+    });
+    
+    const [err, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) =>{
+        setUser(prev=>({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleClick = async e =>{
+        e.preventDefault();
+        if (!user.email || !user.password) {
+            setError('Email and password are required.');
+            return;
+          }
+        try {
+            await axios.post("http://localhost:5000/signup", user);
+            navigate("/login");
+
+        } catch (err) {
+            setError(err.response.data.error);
+        }
+    }
+
+    return (
     <div  class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 
         <div class="w-full bg-white rounded-lg shadow dark md:mt-0 sm:max-w-md xl:p-0 ">
@@ -11,21 +42,21 @@ const SignUp = () => {
                 <form>
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required="" />
+                        <input type="email" onChange={handleChange} name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" required="" />
                     </div>
 
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
+                        <input type="password" onChange={handleChange} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required="" />
                     </div>
 
 
                     <br />
                     <div>
-                        <button type="submit" class="w-full text-white bg-[#0E327A] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign up</button>
-                        
+                        <button type="submit" onClick={handleClick} class="w-full text-white bg-[#0E327A] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign in</button>
+                        {err && <p class="text-center text-[#ff0000]">{err}</p>}
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Have an account? <a href="/login" class="font-medium text-[#EB1402] hover:underline ">Log In</a>
+                            Have an account? <a href="/login" class="font-medium text-[#0E327A] hover:underline ">Log In</a>
                         </p>
                     </div>
 
